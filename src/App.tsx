d5cc8ea2b6eed8b14df044760953d9d014b8178f
mitul72 <mituldhawan2@gmail.com>
2024-06-11 16:49:12 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <h1 className="flex justify-center items-center text-4xl">
+        Welcome to Tauri!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
