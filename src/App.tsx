a938c5ea21374e4a04c4f1d6228556d112da5999
Danny Horning <danny-horning@hotmail.com>
2024-06-19 09:22:22 -0600

    app page converted to login page

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,28 @@ function App() {
   return (
     <div className="min-h-screen">
       <h1 className="flex justify-center items-center text-4xl">
-        Welcome to Tauri!
+        Login
       </h1>
+      <div className="flex justify-center items-center">
+        <form className="flex flex-col space-y-4">
+          <input
+            type="text"
+            placeholder="Username"
+            className="border border-gray-300 p-2"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="border border-gray-300 p-2"
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-white p-2 rounded"
+          >
+            Login
+          </button>
+        </form>
+    </div>
     </div>
   );
 }
