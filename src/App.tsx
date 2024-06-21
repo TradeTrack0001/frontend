9ea505fc68d0100d8e6fcfcb74060590f18fcb34
Danny Horning <danny-horning@hotmail.com>
2024-06-21 08:12:26 -0600

    addition of register button and state
    Added the register button, and based on user state, the user will be able to register or login. Functionality only changes state right now, will need to add in final functionality later

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [login , setLogin] = useState(true);
+
+  const handleRegisterButton = () => {
+    setLogin(false);
+  }
+  const handleLoginButton = () => {
+    setLogin(true);
+  }
   return (
     <div className="min-h-screen">
       <h1 className="flex justify-center items-center text-4xl">
-        Login
+        {login ? "Login" : "Register"}
       </h1>
+      
+    <div className="min-h-screen">
+      
       <div className="flex justify-center items-center">
         <form className="flex flex-col space-y-4">
           <input
@@ -20,11 +32,19 @@ function App() {
           />
           <button
             type="submit"
+            onClick={handleLoginButton}
             className="bg-blue-500 text-white p-2 rounded"
           >
             Login
           </button>
+          <button
+            type = 'button'
+            onClick={handleRegisterButton}
+            className ="bg-blue-500 text-white p-2 rounded">
+            Register
+          </button>
         </form>
+    </div>
     </div>
     </div>
   );
