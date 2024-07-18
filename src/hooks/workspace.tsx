740fd191ae701b11ea4e51778ae0b3925dd063a6
mitul72 <mituldhawan2@gmail.com>
2024-07-18 17:16:00 -0600

    update workspaces

--- src/hooks/workspace.tsx
+++ src/hooks/workspace.tsx
@@ -1,4 +1,6 @@
+import axios from "axios";
 import React, { createContext, useContext, useState, ReactNode } from "react";
+import { useAuth } from "./AuthContext";
 
 // Define the shape of the context state
 type Workspace = {
@@ -8,6 +10,21 @@ type Workspace = {
 interface ContextState {
   currentWorkspace: Workspace | null;
   setCurrentWorkspace: (value: Workspace) => void;
+  getInventory: () => Promise<
+    {
+      itemID: any;
+      itemName: any;
+      itemDescription: any;
+      itemQuantity: any;
+      itemStatus: any;
+      itemSize: any;
+      type: any;
+      checkInDate: any;
+      checkOutDate: any;
+      location: any;
+      workspaceId: any;
+    }[]
+  >;
 }
 
 // Create the context with a default value
@@ -23,9 +40,46 @@ export const WorkspaceProvider: React.FC<MyProviderProps> = ({ children }) => {
   const [currentWorkspace, setCurrentWorkspace] = useState<Workspace | null>(
     null
   );
+  const { auth } = useAuth();
+  const baseUrl = "http://localhost:2000";
+
+  async function getInventory() {
+    const inventoryList: any = [];
+    if (!currentWorkspace) return [];
+    const url = `${baseUrl}/workspace/workspaces/${currentWorkspace?.id}/inventory`;
+
+    try {
+      const res = await axios.get(url, {
+        headers: {
+          Authorization: `Bearer ${auth?.token}`,
+        },
+      });
+      if (res.status === 200) {
+        console.log("Successfully retrieved the object");
+
+        const data = res.data;
+        console.log("This is the data: ", data);
+        if (data && currentWorkspace) {
+          console.log("Successfully parsed");
+          return data;
+        } else {
+          console.log("Failed to parse inventory data");
+          return [];
+        }
+      } else {
+        console.log("Failed to get response");
+        return [];
+      }
+    } catch (e: any) {
+      console.error("Error in getInventory: " + e.toString());
+      return [];
+    }
+  }
 
   return (
-    <Workspace.Provider value={{ currentWorkspace, setCurrentWorkspace }}>
+    <Workspace.Provider
+      value={{ currentWorkspace, setCurrentWorkspace, getInventory }}
+    >
       {children}
     </Workspace.Provider>
   );
