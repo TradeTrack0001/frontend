37ea3ea30e7552d0a70461012a46d1dad2bee8f7
mitul72 <mituldhawan2@gmail.com>
2024-07-18 08:53:40 -0600

    update base url

--- src/hooks/inventory.tsx
+++ src/hooks/inventory.tsx
@@ -1,10 +1,23 @@
 import axios from "axios";
+import { useWorkspace } from "./workspace";
 
-const baseUrl = "http://localhost:2000/api/";
+const baseUrl = "http://localhost:2000";
 
 export default async function getInventory() {
-  const inventoryList: { itemID: any; itemName: any; itemDescription: any; itemQuantity: any; itemStatus: any; itemSize: any; type: any; checkInDate: any; checkOutDate: any; location: any; }[] = [];
-  const url = `${baseUrl}get_products`;
+  const { currentWorkspace, setCurrentWorkspace } = useWorkspace();
+  const inventoryList: {
+    itemID: any;
+    itemName: any;
+    itemDescription: any;
+    itemQuantity: any;
+    itemStatus: any;
+    itemSize: any;
+    type: any;
+    checkInDate: any;
+    checkOutDate: any;
+    location: any;
+  }[] = [];
+  const url = `${baseUrl}/api/get_products`;
 
   try {
     const res = await axios.get(url);
@@ -14,20 +27,33 @@ export default async function getInventory() {
       console.log("This is the data: ", data);
 
       if (data && data.inventory) {
-        data.inventory.forEach((value: { itemID: any; itemName: any; itemDescription: any; itemQuantity: any; itemStatus: any; itemSize: any; type: any; checkInDate: any; checkOutDate: any; location: any; }) => {
-          inventoryList.push({
-            itemID: value.itemID || "No ID",
-            itemName: value.itemName || "No name",
-            itemDescription: value.itemDescription || "No description",
-            itemQuantity: value.itemQuantity || "No quantity",
-            itemStatus: value.itemStatus || "No status",
-            itemSize: value.itemSize || "No size",
-            type: value.type || "No type",
-            checkInDate: value.checkInDate || "No date",
-            checkOutDate: value.checkOutDate || "No date",
-            location: value.location || "No location"
-          });
-        });
+        data.inventory.forEach(
+          (value: {
+            itemID: any;
+            itemName: any;
+            itemDescription: any;
+            itemQuantity: any;
+            itemStatus: any;
+            itemSize: any;
+            type: any;
+            checkInDate: any;
+            checkOutDate: any;
+            location: any;
+          }) => {
+            inventoryList.push({
+              itemID: value.itemID || "No ID",
+              itemName: value.itemName || "No name",
+              itemDescription: value.itemDescription || "No description",
+              itemQuantity: value.itemQuantity || "No quantity",
+              itemStatus: value.itemStatus || "No status",
+              itemSize: value.itemSize || "No size",
+              type: value.type || "No type",
+              checkInDate: value.checkInDate || "No date",
+              checkOutDate: value.checkOutDate || "No date",
+              location: value.location || "No location",
+            });
+          }
+        );
 
         console.log(inventoryList);
         console.log("This is the inventory list");
@@ -45,5 +71,3 @@ export default async function getInventory() {
     return [];
   }
 }
-
-
